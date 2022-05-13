80189039dad07a5a5a2c1a2b117da3702641aa50
apeli23 <apelibrian@gmail.com>
2022-05-13 06:33:08 +0300

    functioning-ripple

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import * as PIXI from 'pixi.js'
 
 
 export default function Home() {
-  let app, image;
+  let app, image, displacementSprite, displacementFilter;
 
   useEffect(() => {
     app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
     document.body.appendChild(app.view);
-    image = new PIXI.Sprite.from("https://res.cloudinary.com/dogjmmett/image/upload/v1652239380/UI_u3vkrp.png");
+    image = new PIXI.Sprite.from("https://res.cloudinary.com/dogjmmett/image/upload/v1652412717/template_vowego.jpg");
     image.width = window.innerWidth;
     image.height = window.innerHeight;
     app.stage.addChild(image);
+
+    displacementSprite = new PIXI.Sprite.from("https://res.cloudinary.com/dogjmmett/image/upload/v1652411299/cloud_eg89xa.png");
+    displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    app.stage.addChild(displacementSprite);
+    app.stage.filters = [displacementFilter];
+
+    animate();      
   })
+
+  function animate() {
+    displacementSprite.x += 10;
+    displacementSprite.y += 4;
+    requestAnimationFrame(animate);
+  }
   return (
     <div>
       <h1>Works</h1>
